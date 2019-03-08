c90bb44f1c53244173e744b5b7f1c0fffca1079d
Ben Ragsdale <ben.ragsdale@gmail.com>
2019-03-08 17:01:41 -0600

    new functs add empl

--- client.js
+++ client.js
@@ -1,25 +1,64 @@
-// The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
 
-//A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields.Using the stored information, calculate monthly costs and append this to the to DOM.If the total monthly cost exceeds $20, 000, add a red background color to the total monthly cost.
+//A 'Submit' button should collect the form information, store the information to calculate monthly costs, append information to the DOM and clear the input fields.Using the stored information, calculate monthly costs and append this to the to DOM.If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
 
-// Create a delete button that removes an employee from the DOM.For Base mode, it does not need to remove that Employee's salary from the reported total.
+
 $(document).ready(readyNow);
 let employeeInfo = [];
 function readyNow() {
 console.log('This is in the readyNow func');
 console.log('employee info var', employeeInfo);
 //function to push val info into array
-takeEmployeeInfo();
+$('#submitButton').on('click', takeEmployeeInfo);
+
 }
 
+//takes employee info in
 function takeEmployeeInfo() {
+//onclick to enter everything
+//clear input fields
+//create obj w/inputs
 let employeeObj = {
-    firstName: 'Ben',
-    lastName: 'Ragsdale',
-    id: 17,
-    title: 'CEO',
-    annualSalary: 345000 
-}
+  firstName: $("#firstNameIn").val(),
+  lastName: $("#lastNameIn").val(),
+  id: $("#idIn").val(),
+    title: $("#titleIn").val(),
+    annualSalary: $("#salaryIn").val()
+};
 employeeInfo.push(employeeObj);
 console.log('employee info arr', employeeInfo);
+//func to add info to tbl
+    addEmployeeInfo();
 }
+
+function addEmployeeInfo(){
+    let el = $("#employeeInfoOutput");
+    el.empty();
+    for (employee of employeeInfo) {
+        el.append(
+          `<tr id="${employee.lastName}">
+          <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+            </tr>`
+        );
+    }
+}
+
+//adds employee info to table
+//create tds for each input
+//append to employeeInfoOutput
+
+
+//calc monthly cost
+//totalMonthyOut
+
+// return salary / 12 ?
+
+
+//add salary to counter 
+//counter += calc monthly cost
+
+
+// Create a delete button that removes an employee from the DOM.For Base mode, it does not need to remove that Employee's salary from the reported total.
